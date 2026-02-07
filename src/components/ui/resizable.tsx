2173e882cb416ab532e11e5845026ab3bca09c4c
paulpopoola <popoolapaultemitope1@gmail.com>
2026-02-07 23:06:34 +0100

    dzffzf

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,33 +1,54 @@
 "use client";
 
 import { GripVertical } from "lucide-react";
-import * as ResizablePrimitive from "react-resizable-panels";
 
 import { cn } from "./utils";
 
+// Simple resizable components without react-resizable-panels dependency
+// You can replace these with actual react-resizable-panels once the correct version is installed
+
+interface ResizablePanelGroupProps extends React.HTMLAttributes<HTMLDivElement> {
+  direction?: "horizontal" | "vertical";
+}
+
 const ResizablePanelGroup = ({
   className,
+  direction = "horizontal",
   ...props
-}: React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelGroup>) => (
-  <ResizablePrimitive.PanelGroup
+}: ResizablePanelGroupProps) => (
+  <div
+    data-panel-group-direction={direction}
     className={cn(
-      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+      "flex h-full w-full",
+      direction === "vertical" && "flex-col",
       className
     )}
     {...props}
   />
 );
 
-const ResizablePanel = ResizablePrimitive.Panel;
+const ResizablePanel = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div 
+    data-slot="resizable-panel"
+    className={cn("flex-1", className)} 
+    {...props} 
+  />
+);
+
+interface ResizableHandleProps extends React.HTMLAttributes<HTMLDivElement> {
+  withHandle?: boolean;
+}
 
 const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelResizeHandle> & {
-  withHandle?: boolean;
-}) => (
-  <ResizablePrimitive.PanelResizeHandle
+}: ResizableHandleProps) => (
+  <div
+    data-slot="resizable-handle"
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -39,7 +60,7 @@ const ResizableHandle = ({
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </ResizablePrimitive.PanelResizeHandle>
+  </div>
 );
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
